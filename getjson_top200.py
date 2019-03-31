d289c13891272f3c291f146d099549ba9ec1a048
kentaojapi <kentaojapi@gmail.com>
2019-03-31 12:02:38 +0900

    update getjson_top200.py

--- getjson_top200.py
+++ getjson_top200.py
@@ -21,9 +21,16 @@ class SpotifyToken(object):
         self.bucket_name = 'spotify-analysis'
 
 
-def get_top200csv(project_name=user.project_name, bucket_name=user.bucket_name):
-    source_blob_name = "top200.csv"
-    destination_file_name = "top200.csv"
+def get_new_access_token(oauth):
+    refresh_token = REFLESH_TOKEN
+    new_token = oauth.refresh_access_token(refresh_token)
+    access_token = new_token['access_token']
+    return access_token
+
+
+def get_top200csv(project_name, bucket_name):
+    source_blob_name = "top200_nonduplicate.csv"
+    destination_file_name = "top200_nonduplicate.csv"
     storage_client = storage.Client(project_name)
     bucket = storage_client.get_bucket(bucket_name)
     blob = bucket.blob(source_blob_name)
@@ -39,60 +46,68 @@ def load_csv(csv_file_pass):
 
 
 def get_audio_features(sp, search_word):
-    '''
-    Get Audio Features.
-    '''
-    try:
-        # search by artist name
-        name = search_word    
-        result = sp.search(q='artist:' + name, type='artist')
+    #try:
+    # search by artist name
+    name = search_word    
+    result = sp.search(q='artist:' + name, type='artist')
 
-        # get the artist_id
-        artist_id = result['artists']['items'][0]['id']
+    # get the artist_id
+    artist_id = result['artists']['items'][0]['id']
 
-        # get the latest album of the artist
-        albums = sp.artist_albums(artist_id)
+    # get the latest album of the artist
+    albums = sp.artist_albums(artist_id)
 
-         # get the ids of their albumns
-        albums_ids = []
-        albums_count = 0
-        for items in albums['items']:
-            albums_ids.append(items['id'])
-            albums_count += 1
+     # get the ids of their albumns
+    albums_ids = []
+    albums_count = 0
+    for items in albums['items']:
+        albums_ids.append(items['id'])
+        albums_count += 1
 
-        # get json of information about all songs
-        songs_list = []
-        for ids in albums_ids:
-            album_tracks = sp.album_tracks(ids)
-            songs_list.append(album_tracks)
+    # get json of information about all songs
+    songs_list = []
+    for ids in albums_ids:
+        album_tracks = sp.album_tracks(ids)
+        songs_list.append(album_tracks)
 
-        # get ids and names of all songs
-        songs_ids = []
-        songs_names = []
-        for i in range(albums_count):
-            for j in range(len(songs_list[i]['items'])):
-                songs_ids.append(songs_list[i]['items'][j]['id'])
-                songs_names.append(songs_list[i]['items'][j]['name'])
+    # get ids and names of all songs
+    songs_ids = []
+    songs_names = []
+    for i in range(albums_count):
+        for j in range(len(songs_list[i]['items'])):
+            songs_ids.append(songs_list[i]['items'][j]['id'])
+            songs_names.append(songs_list[i]['items'][j]['name'])
 
-        # get Audio Features of their songs
-        audio_features_json = []
-        for song_id in songs_ids:
-            audio_feature = sp.audio_features(tracks=song_id)
-            audio_features_json.append(audio_feature[0])
+    # get Audio Features of their songs
+    songs_ids = list(split_list(songs_ids, 20))
+    audio_features_json = []
+    for songs20 in songs_ids:
+        audio_features_20 = sp.audio_features(tracks=songs20)
+        for audio_feature in audio_features_20:
+            audio_features_json.append(audio_feature)
 
-        # make pandas.DataFrame from json
-        df_features = pd.DataFrame(data=audio_features_json)
-        df = pd.concat([df_features, pd.Series(songs_names).rename('name')], axis=1)
-        df.to_csv('./df-audio-features/audio_features_{}.csv'.format(search_word), index=False)
+    # make dataframe
+    df_features = pd.DataFrame(data=audio_features_json)
+    df = pd.concat([df_features, pd.Series(songs_names).rename('name')], axis=1)
+    df = df.assign(Artist=search_word)
+    print("get audio features of the songs of {}".format(search_word))
+    return df
 
-        # post to GCS.
-        client = storage.Client("atomic-lens-188216")
-        bucket = client.get_bucket('spotify-analysis')
-        blob = bucket.blob('audio-features_{}.csv'.format(search_word))
-        blob.upload_from_filename(filename='df-audio-features/audio_features_{}.csv'.format(search_word))
+    #except:
+    #    print("Could not find the artist.")
+    #    return None
 
-    except:
-        print("Could not find the artist.")
+
+def split_list(songs_ids, n):
+    for idx in range(0, len(songs_ids), n):
+        yield songs_ids[idx:idx + n]
+
+
+def upload_csv(filepath):
+    client = storage.Client("atomic-lens-188216")
+    bucket = client.get_bucket('spotify-analysis')
+    blob = bucket.blob('top200_audiofeatures.csv')
+    blob.upload_from_filename(filename=filepath)
 
 
 def get_new_access_token(oauth):
@@ -109,9 +124,14 @@ if __name__ == '__main__':
     access_token = get_new_access_token(oauth)
     sp = spotipy.Spotify(auth=access_token)
     sp.trace = False
-    csv_file_path = get_top200csv()
+    csv_file_path = get_top200csv(user.project_name, user.bucket_name)
     artists = load_csv(csv_file_path)
-    df_features_top200
+    df_features_top200 = pd.DataFrame()
     for artist in artists:
-        df_audiofearture_artist = get_audio_features(sp, search_word=artist)
+        print(artist[0])
+        df_audiofeature_artist = get_audio_features(sp, search_word=artist[0])
+        df_features_top200 = pd.concat([df_features_top200, df_audiofeature_artist])
+    top200_audiofeatures_csv = 'df-audio-features/top200_audiofeatures.csv'
+    df_features_top200.to_csv(top200_audiofeatures_csv)
+    upload_csv(top200_audiofeatures_csv)
 
